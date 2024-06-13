d512b654d03fb6391bb5e2c8c28b35041162b5a2
Void <130793393+thetrulyvoid@users.noreply.github.com>
2024-06-13 12:28:34 -0300

    Add files via upload

---
+++ PokemonUser.h
@@ -0,0 +1,11 @@
+#ifndef PokemonUSER_H
+#define PokemonUSER_H
+
+    void victini();
+    void bulbasaur();
+    void eevee();
+    void victiniFront();
+    void bulbasaurFront();
+    void eeveeFront();
+
+#endif
---
+++ statusPoke.h
@@ -0,0 +1,48 @@
+#ifndef Statuspoke_H
+#define Statuspoke_H
+
+    typedef struct {
+
+        int HP;
+        int Attack;
+        int Defense;
+        int SpAtk;
+        int SpDef;
+        int Seepd;
+    //SKILLS
+        //int Covet = 60;
+        //int Swift = 60; //60% de acerto
+        //int Charm = 100; // eevee sempre acerta e diminui o dano de seu oponente  
+    } EeveeSt, BulbasaurSt, VictiniSt;
+
+
+
+    //typedef struct {
+
+      //  int HP = 45;
+      //  int Attack = 49;
+      //  int Defense = 49;
+      //  int SpAtk = 65;
+       // int SpDef = 65;
+       // int Speed = 45;
+    //SKILLS
+      //  int Tackle = 40;
+       // int PoisonPowder = 100; //bulba sempre acerta debuff de dano no oponente
+      //  int SolarBeam = 60; //60% de acerto
+   // } BulbasaurSt;
+
+   // typedef struct {
+    //QUE ISSUUUUUU!
+     //   int HP = 100;
+    //    int Attack = 100;
+    //    int Defense = 100;
+      //  int SpAtk = 100;
+     //   int SpDef = 100;
+     //   int Speed = 100;
+    //SKILLS
+       // int Confusion = 50; // 50% de acerto
+     //   int QuickAttack = 40;
+      //  int Flame = 50; //debuff
+
+   // } VictiniSt;
+    #endif 
